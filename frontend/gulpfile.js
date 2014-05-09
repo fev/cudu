b6ca36f520209a9b71c028679d1cdd3743222ada
Luis Belloch <luisbelloch@gmail.com>
2014-05-09 20:56:53 +0200

    Mejoras compilación frontend.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,86 +2,37 @@
 'use strict';
 
 var gulp = require('gulp'),
-    bump = require('gulp-bump'),
     bowerfiles = require('gulp-bower-files'),
-    cache = require('gulp-cache'),
-    changed = require('gulp-changed'),
+    bump = require('gulp-bump'),
     clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
     crypto = require('crypto'),
     es = require('event-stream'),
+    filter = require('gulp-filter'),
     googlecdn = require('gulp-google-cdn'),
     gulpif = require('gulp-if'),
     htmlmin = require('gulp-htmlmin'),
-    inject = require('gulp-inject'),
     jshint = require('gulp-jshint'),
     livereload = require('gulp-livereload'),
     minifycss = require('gulp-minify-css'),
     path = require('path'),
     rename = require('gulp-rename'),
     replace = require('gulp-replace'),
-    sass = require('gulp-ruby-sass'),
-    uglify = require('gulp-uglify');
-
-var opts = require('yargs').argv;
+    revreplace = require('gulp-rev-replace'),
+    uglify = require('gulp-uglify'),
+    useref = require('gulp-useref');
 
 var revision = function() {
   return es.map(function(file, callback) {
     var contents = file.contents.toString();
     var hash = crypto.createHash('md5').update(contents, 'utf8').digest('hex');
 		var filename = hash.slice(0, 8) + '.' + path.basename(file.path);
+		file.revOrigPath = file.path;
+		file.revOrigBase = file.base;
 		file.path = path.join(path.dirname(file.path), filename);
     callback(null, file);
   });
 };
-
-gulp.task('styles', function() {
-  return gulp.src('app/styles/cudu.scss')
-    .pipe(revision())
-    .pipe(sass({ style: 'compact' }))
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/styles'));
-});
-
-gulp.task('scripts', function() {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('cudu.js'))
-    .pipe(revision())
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'));
-});
-
-gulp.task('bower-files', function(){
-    bowerfiles().pipe(gulp.dest('dist/lib'));
-});
-
-gulp.task('inject', ['styles', 'scripts'], function() {
-  var source = ['dist/scripts/cudu.min.js', 'dist/styles/*cudu.min.css'];
-  if (opts.readable) {
-    source = ['dist/scripts/cudu.js', 'dist/styles/*cudu.css'];
-  }
-  return gulp.src('app/index.html')
-    .pipe(gulpif(!opts.readable, googlecdn(require('./bower.json'), { componentsPath: 'lib' })))
-    .pipe(gulpif(!opts.readable, replace('bootstrap.', 'bootstrap.min.')))
-    .pipe(inject(gulp.src(source, { read: false }), { addRootSlash: false, ignorePath: '/dist/' }))
-    .pipe(gulpif(!opts.readable, htmlmin({ removeComments: true, collapseWhitespace: true })))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function() {
-  return gulp.src(['dist/**'], {read: false})
-    .pipe(clean());
-});
-
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'inject', 'bower-files');
-});
 
 gulp.task('bump', function () {
   return gulp.src(['./package.json', './bower.json'])
@@ -89,14 +40,66 @@ gulp.task('bump', function () {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('clean', function() {
+  return gulp.src(['dist/**', 'app/styles/cudu.css'], { read: false })
+    .pipe(clean());
+});
+
+gulp.task('compass', function() {
+  return gulp.src('app/styles/cudu.scss')
+    .pipe(compass({
+      project: path.join(__dirname, 'app'),
+      css: 'styles', sass: 'styles', image: 'images',
+      style: 'expanded' 
+    }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('app/styles'));
+});
+
+gulp.task('bower-files', function() {
+  return bowerfiles().pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*').pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('preflight', ['compass', 'bower-files', 'images'], function() {
+  var stylesFilter  = filter('**/*.css');
+  var scriptsFilter = filter('**/*.js');  
+  return gulp.src('app/**/*.html')
+    .pipe(useref.assets())
+    .pipe(scriptsFilter)
+      .pipe(revision())
+      .pipe(uglify())
+      .pipe(rename({ suffix: '.min' }))
+    .pipe(scriptsFilter.restore())
+    .pipe(stylesFilter)
+      .pipe(revision())
+      .pipe(rename({ suffix: '.min' }))
+    .pipe(stylesFilter.restore())
+    .pipe(useref.restore())
+    .pipe(useref())
+    .pipe(revreplace())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['preflight'], function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(gulpif(!opts.readable, googlecdn(require('./bower.json'), { componentsPath: 'lib' })))
+    .pipe(replace(
+      'lib/bootstrap/dist/css/bootstrap.css', 
+      '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.html', ['inject']);
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/images/**/*', ['images']);
+  gulp.watch('app/styles/**/*.scss', ['compass']);
 
   var server = livereload();
   gulp.watch(['dist/**']).on('change', function(file) {
     server.changed(file.path);
   });
 });
+
+
