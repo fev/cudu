50a8f8515f3bd1be3c2fef6c11e55b6fb9631e42
Luis Belloch <luisbelloch@gmail.com>
2014-11-16 18:12:37 +0100

    Corregida longitud identificador de grupo.

--- backend/src/main/java/org/scoutsfev/cudu/domain/Grupo.java
+++ backend/src/main/java/org/scoutsfev/cudu/domain/Grupo.java
@@ -3,9 +3,7 @@ package org.scoutsfev.cudu.domain;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.URL;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -17,6 +15,8 @@ import java.util.List;
 public class Grupo {
 
     @Id
+    @Column(length = 3)
+    @Size(min = 2, max = 3)
     private String id;
 
     @OneToMany(mappedBy = "grupo")
