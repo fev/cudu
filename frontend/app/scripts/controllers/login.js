7f693e7a7084bf8aacc9862b9695880bd7151893
Luis Belloch <luisbelloch@gmail.com>
2015-01-01 18:05:18 +0100

    Formulario básico de login, solo estilos.

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -2,13 +2,22 @@
 
 angular.module('cuduApp').controller('LoginCtrl', ['$scope', '$http', 'Usuario', function ($scope, $http, Usuario) {
 
+  $scope.error = null;
+  $scope.mayusculas = false;
+
+  $scope.detectarMayusculas = function(e) {
+    var keyCode = e.keyCode ? e.keyCode : e.which;
+    var shiftKey = e.shiftKey ? e.shiftKey : ((keyCode == 16) ? true : false);
+    $scope.mayusculas = (((keyCode >= 65 && keyCode <= 90) && !shiftKey) || ((keyCode >= 97 && keyCode <= 122) && shiftKey));
+  };
+
   $scope.login = function() {
     var email = "baden@scoutsfev.org";
     var password = "1234";
 
     Usuario.autenticar(email, password)
       .success(function(status, data) {
-        
+
       })
       .error(function(status, data) {
 
