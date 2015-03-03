ad9fd901b437bb87b042e3a2ab671bebffcc83f2
Luis Belloch <luisbelloch@gmail.com>
2015-03-03 22:40:18 +0100

    Dependencia de Compass mediante Bundler.

---
+++ frontend/config.rb
@@ -0,0 +1,11 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "app/styles"
+sass_dir = "app/styles"
+images_dir = "app/images"
+javascripts_dir = "app/scripts"
+
+# :expanded, :nested, :compact, :compressed
+output_style = :compressed
+line_comments = false
