fd5d3897dcfbd4d8042af5c7accca0be4662a122
juanramos <jramosbalaguer@gmail.com>
2015-06-18 23:51:22 +0200

    Devolver archivo generado desde el controllador de fichas

--- backend/src/main/java/org/scoutsfev/cudu/web/FichaController.java
+++ backend/src/main/java/org/scoutsfev/cudu/web/FichaController.java
@@ -1,14 +1,23 @@
 package org.scoutsfev.cudu.web;
 
-
+import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.scoutsfev.cudu.domain.Ficha;
 import org.scoutsfev.cudu.services.FichaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.zeroturnaround.zip.commons.FileUtils;
+import org.zeroturnaround.zip.commons.FilenameUtils;
 
-import java.util.ArrayList;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.util.List;
 
 @RestController
@@ -25,33 +34,48 @@ public class FichaController {
     }
 
     @RequestMapping(value = "/ficha/{idFicha}/generar", method = RequestMethod.POST)
-    public String GenerarReport(@PathVariable Integer idFicha, @RequestBody List<Integer> asociados) {
+    public void GenerarReport(@PathVariable Integer idFicha, @RequestBody List<Integer> asociados,
+                              HttpServletResponse response) throws IOException, COSVisitorException {
         try {
 
-            return reportingService.GenerarFicha(asociados, null, null, idFicha, "es"); //TODO: obtener lenguaje, del user?
+            String pathArchivo = reportingService.GenerarFicha(asociados, null, null, idFicha, "es"); //TODO: obtener lenguaje, del user?
+            DevolverArchivo(pathArchivo, response);
 
         } catch (Exception ex) {
 
             logger.error("Error generando fichas");
-            return "";
+            throw ex;
         }
     }
 
     @RequestMapping(value = "/ficha/{idFicha}/actividad/{actividadId}/generar", method = RequestMethod.POST)
-    public String GenerarReport(@PathVariable Integer idFicha, @PathVariable Integer actividadId, @RequestBody List<Integer> asociados) {
+    public void GenerarReport(@PathVariable Integer idFicha, @PathVariable Integer actividadId, @RequestBody List<Integer> asociados,
+                              HttpServletResponse response) throws IOException, COSVisitorException {
         try {
 
-            return reportingService.GenerarFicha(asociados, actividadId, null, idFicha, "es"); //TODO: obtener lenguaje, del user?
+            String pathArchivo = reportingService.GenerarFicha(asociados, actividadId, null, idFicha, "es"); //TODO: obtener lenguaje, del user
+            DevolverArchivo(pathArchivo, response);
 
         } catch (Exception ex) {
 
-            logger.error("Error generando fichas");
-            return "";
+            logger.error("Error generando fichas: {0}", ex.getMessage());
+            throw ex;
         }
     }
 
     @RequestMapping(value = "/fichas/lenguaje/{lenguajeId}/tipo/{tipoFicha}", method = RequestMethod.GET)
     public List<Ficha> ObtenerFichas(@PathVariable String lenguajeId, @PathVariable int tipoFicha) {
         return reportingService.ObtenerFichas(lenguajeId, tipoFicha);
+    }
+
+    private void DevolverArchivo(String pathArchivo, HttpServletResponse response) throws IOException {
+
+        File archivo = new File(pathArchivo);
+
+        response.setContentType(URLConnection.guessContentTypeFromName(pathArchivo));
+        response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", archivo.getName()));
+
+        FileUtils.copy(archivo, response.getOutputStream());
+        response.flushBuffer();
     }
 }
