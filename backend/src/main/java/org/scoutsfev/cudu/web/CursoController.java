c5716f464a9b42e827692c405bc9470ba0eba114
Luis Belloch <luisbelloch@gmail.com>
2015-08-28 10:51:07 +0200

    Habilitadas anotaciones de seguridad en controlador de cursos.

--- backend/src/main/java/org/scoutsfev/cudu/web/CursoController.java
+++ backend/src/main/java/org/scoutsfev/cudu/web/CursoController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,13 +27,13 @@ public class CursoController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public Page<Curso> listado(@AuthenticationPrincipal Usuario usuario, Pageable pageable) {
         return cursoService.listado(pageable);
     }
 
     @RequestMapping(value = "/{cursoId}", method = RequestMethod.GET)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public ResponseEntity<Curso> obtener(@PathVariable("cursoId") Integer cursoId, @AuthenticationPrincipal Usuario usuario) {
         Curso curso = cursoService.obtener(cursoId);
         if (curso == null)
@@ -42,13 +43,13 @@ public class CursoController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public Curso crear(@RequestBody @Valid Curso curso, @AuthenticationPrincipal Usuario usuario) {
         return cursoService.guardar(curso);
     }
 
     @RequestMapping(value = "/{cursoId}", method = RequestMethod.PUT)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public Curso editar(@RequestBody @Valid Curso editado, @PathVariable("cursoId") Integer cursoId, @AuthenticationPrincipal Usuario usuario) {
         editado.setId(cursoId);
         return cursoService.guardar(editado);
@@ -59,25 +60,25 @@ public class CursoController {
     // @RequestMapping(value = "/{id}/ocultar", method = RequestMethod.PUT)
 
     @RequestMapping(value = "/{cursoId}/formadores", method = RequestMethod.POST)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public void añadirFormador(@PathVariable("cursoId") Integer cursoId, @RequestBody @Valid @NotNull Integer asociadoId, @AuthenticationPrincipal Usuario usuario) {
         cursoService.añadirFormador(cursoId, asociadoId);
     }
 
     @RequestMapping(value = "/{cursoId}/formadores/{asociadoId}", method = RequestMethod.DELETE)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public void quitarFormador(@PathVariable("cursoId") Integer cursoId, @PathVariable("asociadoId") Integer asociadoId, @AuthenticationPrincipal Usuario usuario) {
         cursoService.quitarFormador(cursoId, asociadoId);
     }
 
     @RequestMapping(value = "/{cursoId}/participantes", method = RequestMethod.POST)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public void añadirParticipante(@PathVariable("cursoId") Integer cursoId, @RequestBody @Valid @NotNull Integer asociadoId, @AuthenticationPrincipal Usuario usuario) {
         cursoService.añadirParticipante(cursoId, asociadoId);
     }
 
     @RequestMapping(value = "/{cursoId}/participantes/{asociadoId}", method = RequestMethod.DELETE)
-    // @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
+    @PreAuthorize("@auth.puedeAccederLluerna(#usuario)")
     public void quitarParticipante(@PathVariable("cursoId") Integer cursoId, @PathVariable("asociadoId") Integer asociadoId, @AuthenticationPrincipal Usuario usuario) {
         cursoService.quitarParticipante(cursoId, asociadoId);
     }
