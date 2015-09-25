c1a9ed083a70536e8d75e3bb80e0e7f57a35ea1d
Luis Belloch <luisbelloch@gmail.com>
2015-09-25 21:40:25 +0200

    Método para obtener un listado de cursos en el proceso de inscripción

--- backend/src/main/java/org/scoutsfev/cudu/storage/CursoStorage.java
+++ backend/src/main/java/org/scoutsfev/cudu/storage/CursoStorage.java
@@ -1,7 +1,13 @@
 package org.scoutsfev.cudu.storage;
 
+import org.scoutsfev.cudu.domain.Curso;
 import org.scoutsfev.cudu.domain.EstadoInscripcionEnCurso;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CursoStorage {
+    List<Curso> listado(Pageable pageable, Optional<Boolean> visibles);
     EstadoInscripcionEnCurso estadoDeInscripcion(int cursoId, int asociadoId);
 }
--- backend/src/main/java/org/scoutsfev/cudu/storage/CursoStorageImpl.java
+++ backend/src/main/java/org/scoutsfev/cudu/storage/CursoStorageImpl.java
@@ -1,13 +1,19 @@
 package org.scoutsfev.cudu.storage;
 
-import org.jooq.DSLContext;
-import org.jooq.Result;
+import org.jooq.*;
+import org.scoutsfev.cudu.domain.Curso;
 import org.scoutsfev.cudu.db.tables.records.DtoEstadoInscripcionRecord;
 import org.scoutsfev.cudu.domain.EstadoInscripcionEnCurso;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import static org.jooq.impl.DSL.val;
+import java.util.List;
+import java.util.Optional;
+
+import static org.jooq.impl.DSL.*;
+import static org.scoutsfev.cudu.db.tables.Curso.CURSO;
+import static org.scoutsfev.cudu.db.tables.CursoParticipante.CURSO_PARTICIPANTE;
 import static org.scoutsfev.cudu.db.tables.DtoEstadoInscripcion.DTO_ESTADO_INSCRIPCION;
 
 @Service
@@ -18,6 +24,38 @@ public class CursoStorageImpl implements CursoStorage {
     @Autowired
     public CursoStorageImpl(DSLContext context) {
         this.context = context;
+    }
+
+    public List<Curso> listado(Pageable pageable, Optional<Boolean> visibles) {
+        /*
+         * select c.*, coalesce(ct.inscritos, 0) inscritos
+         * from curso c
+         * left join (
+         *  select c.curso_id, count(c.asociado_id) inscritos from curso_participante c group by c.curso_id) ct
+         * on ct.curso_id = c.id;
+         */
+        SelectHavingStep<Record2<Integer, Integer>> numeroParticipantes = context
+                .select(CURSO_PARTICIPANTE.CURSO_ID, count().as("inscritos"))
+                .from(CURSO_PARTICIPANTE)
+                .groupBy(CURSO_PARTICIPANTE.CURSO_ID);
+
+        Condition clausulaVisibles = val(true).equal(true);
+        if (visibles.isPresent())
+            clausulaVisibles = clausulaVisibles.and(CURSO.VISIBLE.eq(val(visibles.get())));
+
+        final SelectForUpdateStep<Record> query = context
+                .select(CURSO.fields())
+                .select(coalesce(numeroParticipantes.field("inscritos", Integer.class), val(0)).as("inscritos"))
+                .from(CURSO)
+                .leftOuterJoin(numeroParticipantes)
+                    .on(numeroParticipantes.field(CURSO_PARTICIPANTE.CURSO_ID.getName(), Integer.class)
+                            .eq(CURSO.ID))
+                .where(clausulaVisibles)
+                .orderBy(CURSO.ID.desc())
+                .limit(pageable.getPageSize())
+                .offset(pageable.getPageNumber());
+
+        return query.fetchInto(Curso.class);
     }
 
     @Override
