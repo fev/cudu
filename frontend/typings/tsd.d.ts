6a4987a1d36ab1a06df5303b24bbb89c5a197fcf
Luis Belloch <luisbelloch@gmail.com>
2015-09-28 20:35:11 +0200

    Typings typescript para momentjs

--- frontend/typings/tsd.d.ts
+++ frontend/typings/tsd.d.ts
@@ -2,3 +2,5 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angularjs/angular-resource.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
