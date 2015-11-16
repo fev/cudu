126e17315601bac6e33e4f08f71c7f627b2bb08f
Luis Belloch <luisbelloch@gmail.com>
2015-11-17 00:11:05 +0100

    Corregido problema con el usuario actual en frontend al no resolver la promesa correctamente en los casos en que caduca la sesión

--- frontend/app/scripts/services.js
+++ frontend/app/scripts/services.js
@@ -166,8 +166,10 @@ cuduServices.factory('Usuario', ['$http', '$cookies', '$q', function($http, $coo
   svc.autenticar = function(email, password, captcha) {
     delete $cookies['JSESSIONID'];
     var respuesta = $http.post('/api/usuario/autenticar', { 'email': email, 'password': password, 'captcha': captcha });
-    respuesta.success(function(data, status) { svc.usuario = data; });
-    return respuesta;
+    return respuesta.success(function(data, status) {
+      svc.usuario = data;
+      usuarioDiferido.resolve(data);
+    });
   };
 
   svc.desautenticar = function() {
